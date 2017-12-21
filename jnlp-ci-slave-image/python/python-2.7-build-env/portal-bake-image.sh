c57f99233b0b59dfb90213d440a3e688ff7ae338
WingkaiHo <13533512245@163.com>
2017-12-21 16:24:52 +0800

    update bake image interface

--- jnlp-ci-slave-image/python/python-2.7-build-env/portal-bake-image.sh
+++ jnlp-ci-slave-image/python/python-2.7-build-env/portal-bake-image.sh
@@ -2,7 +2,7 @@
 
 GIT_COMMIT_ID=$(git rev-parse --short HEAD)
 
-RESULT_STR=$(curl -s -b cookiec.txt -c cookiec.txt -X POST  "http://$PAAS_PORTAL_HOME/api/cd/version" --form imageFile=@ROOT.tar --form imageName=$DOCKER_IMAGE_NAME --form versionName=$DOCKER_IMAGE_VERSION-$GIT_COMMIT_ID --form runtimeEnvId=$RUNTIME_ENV_ID )
+RESULT_STR=$(curl -s -b cookiec.txt -c cookiec.txt -X POST  "http://$PAAS_PORTAL_HOME/api/cd/version" --form imageFile=@ROOT.tar --form imageName=$DOCKER_IMAGE_NAME --form versionName=$DOCKER_IMAGE_VERSION-$GIT_COMMIT_ID --form runtimeEnvId=$RUNTIME_ENV_ID --form key=$DOCKER_IMAGE_BUILDING_KEY)
 
 CODE=$(echo $RESULT_STR | awk -F "[{,}:]" '{print $3}')
 
